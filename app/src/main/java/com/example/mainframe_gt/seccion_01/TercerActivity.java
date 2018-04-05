3b05680bcdd33c75116e815cb5f2dac2b2d8b003
KildrenVX <migux07@gmail.com>
2018-04-05 02:48:30 -0300

    Sobreescribir metodo

--- app/src/main/java/com/example/mainframe_gt/seccion_01/TercerActivity.java
+++ app/src/main/java/com/example/mainframe_gt/seccion_01/TercerActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +14,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TercerActivity extends AppCompatActivity {
 
     EditText editTextPhone, editTextWeb;
     ImageButton Web, Phone, Camera;
+    TextView textView ;
     private final int PHONE_CALL_CODE = 100;
 
     @Override
@@ -26,23 +29,55 @@ public class TercerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercer);
         editTextPhone = (EditText) findViewById(R.id.editTextPhone);
-        editTextWeb = (EditText) findViewById(R.id.editTextWeb);
+        editTextWeb = (EditText) findViewById(R.id.editTextWebs);
         Web = (ImageButton) findViewById(R.id.imageButtonWeb);
         Phone = (ImageButton) findViewById(R.id.imageButtonPhone);
         Camera = (ImageButton) findViewById(R.id.imageButtonCameta);
+        textView = (TextView) findViewById(R.id.textView);
 
         Phone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // se recoge el numero de telefono
                 String number = editTextPhone.getText().toString();
-                if (number != null) {
+                //pergunta si es nulo o esta vacio
+                if (number != null && !number.isEmpty()) {
                     //Comprobar la version actual de nuestro Android que estamos ejecutando
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+                        //Comprobar si aceptado
+                        if (CheckPermission(Manifest.permission.CALL_PHONE)) {
+                            //acepto
+                            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel: " + number));
+                            if (ActivityCompat.checkSelfPermission(TercerActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                return;}
+                            startActivity(intent);
+                        }else
+                        {
+                            //no acepto o es la primera vez que se pregunta
+                            if(!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE))
+                            {
+                                // no se pregunto aun
+                                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+                            }else {
+                                //a denegado
+                                Toast.makeText(TercerActivity.this, "DEnegado su acceso", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                i.addCategory(Intent.CATEGORY_DEFAULT);
+                                i.setData(Uri.parse("package:"+getPackageName()));//vemps el nombre de nuestra app
+                                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                startActivity(i);
+
+                            }
+                        }
+                        //
                     } else {
                         OldVersion(number);
                     }
 
+                }else {
+                    Toast.makeText(TercerActivity.this, "Vaalor nulo", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -50,6 +85,7 @@ public class TercerActivity extends AppCompatActivity {
             @SuppressLint("MissingPermission")
             private void OldVersion(String phoneNumer) {
                 Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel: " + phoneNumer));
+                //comprovamo se el permiso esta en el manifest
                 if (CheckPermission(Manifest.permission.CALL_PHONE)) {
                     startActivity(intentCall);
                 } else {
@@ -58,6 +94,25 @@ public class TercerActivity extends AppCompatActivity {
             }
 
 
+        });
+
+        Web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = editTextWeb.getText().toString();
+                textView.setText(url);
+
+                if (url!=null&&!url.isEmpty())
+                {
+                    Intent Iweb = new Intent(Intent.ACTION_VIEW,Uri.parse("http://"+url));
+                    //Iweb.setAction(Intent.ACTION_VIEW);
+                    //Iweb.setData(Uri.parse("https://"+url));
+                    startActivity(Iweb);
+                }else {
+                    Toast.makeText(TercerActivity.this, "Error en la URL", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
 
     }
@@ -70,7 +125,7 @@ public class TercerActivity extends AppCompatActivity {
             case PHONE_CALL_CODE:
                 String permission = permissions[0];
                 int result = grantResults[0];
-
+                //ve si el permiso es igual al manifest
                 if (permission.equals(Manifest.permission.CALL_PHONE)) {
                     //Comprobar si acepto el permiso o no
                     if (result == PackageManager.PERMISSION_GRANTED) {
@@ -98,7 +153,7 @@ public class TercerActivity extends AppCompatActivity {
 
     }
 
-    //aqui comprueba un permiso
+    //aqui comprueba un permiso(es un metodo generico)
     private boolean CheckPermission(String permission){
         int  result =this.checkCallingOrSelfPermission(permission);
         return  result == PackageManager.PERMISSION_GRANTED;
